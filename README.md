93567925c1132d4850cf77b6ad3965b42a49d1d0
Evgeny Shestakov <evgeny.shestakov@gmail.com>
2017-07-28 10:49:19 +0000

    implementation with docs

--- README.md
+++ README.md
@@ -1,17 +1,46 @@
+# uses
+
+- json
+- sys
+- os.path
+- math.sqrt
+
 # Ближайшие бары
 
-[FIXME. Здесь будет описание проекта]
+Скрипт использует открытые данные в json формате о барах Москвы
+и выводит наибольший, наименьший бары по количеству мест,
+а так же ищет ближайший к введенным координатам.
+
+Функции:
+
+- load_jsonfile(filepath)
+загружает файл в json формате с исходными данными
+
+- get_biggest_bar(json_input)
+принимает на вход json данные и ищет словарь с информацией о самом вместительном баре
+
+- get_smallest_bar(json_input)
+принимает на вход json данные и ищет словарь с информацией о самом маленьком (по количеству мест) баре
+
+- get_closest_bar(json_input, longitude, latitude)
+принимает на вход json данные а так же координаты: долготу и ширину. Выводит словарь с информацией о баре
 
 # Как запустить
 
 Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+файл с данными доступен по ссылке: https://op.mos.ru/EHDWSREST/catalog/export/get?id=84505
+json (zip)
 
 Запуск на Linux:
 
 ```#!bash
 
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+$ python bars.py filepath longitude latitude # possibly requires call of python3 executive instead of just python
+python bars.py data.json 37 55                                                                                                                                                  
+
+Biggest bar is: "Спорт бар «Красная машина»" with ID: 00138530, seats: 450                                                                                                                                          
+Smallest bar is: "БАР. СОКИ", with ID: 00107283, seats: 0                                                                                                                                                           
+Nearest bar is: "Staropramen" with ID: 00146638, seats: 50
 
 ```
 
--- bars.py
+++ bars.py
@@ -4,10 +4,10 @@ import os.path
 from math import sqrt
 
 
-def load_filedata(filepath):
+def load_jsonfile(filepath):
     if os.path.isfile(filepath):
         with open(filepath, 'r', encoding='cp1251', 
-                    errors='ignore') as file_handler:
+                errors='ignore') as file_handler:
             try:              
                 return json.load(file_handler)
             except ValueError:
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     json_input = None;
     
     try: 
-        json_input = load_filedata(sys.argv[1])
+        json_input = load_jsonfile(sys.argv[1])
     except IndexError:
         print('warning: please add valid json file as argument: ' + 
             'python pprint_json.py data.json')
@@ -61,7 +61,19 @@ if __name__ == '__main__':
     if json_input is not None:
         biggest_bar = get_biggest_bar(json_input)
         smallest_bar = get_smallest_bar(json_input)
-        closest_bar = get_closest_bar(json_input, 37, 55.84614)
-        print(biggest_bar)
-        print(smallest_bar)
-        print(closest_bar)
+        print('Biggest bar is: "{0}" with ID: {1}, seats: {2}'
+            .format(biggest_bar['Name'], biggest_bar['ID'],
+                    str(biggest_bar['SeatsCount'])))
+        print('Smallest bar is: "{0}", with ID: {1}, seats: {2}'
+            .format(smallest_bar['Name'], smallest_bar['ID'],
+                    str(smallest_bar['SeatsCount'])))
+        try:
+            closest_bar = get_closest_bar(json_input, float(sys.argv[2]),
+                                          float(sys.argv[3]))
+            print('Nearest bar is: "{0}" with ID: {1}, seats: {2}'
+                  .format(closest_bar['Name'], closest_bar['ID'],
+                          str(closest_bar['SeatsCount'])))
+        except (IndexError, ValueError):
+            print('warning: please add valid longitude latitude'
+                 'as arguments, like:' +
+                 'python bars.py data.json longitude latitude')
