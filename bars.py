93670972c9f734294debe5d254fbb65f40968043
Evgeny Shestakov <evgeny.shestakov@gmail.com>
2017-08-02 08:31:05 +0000

    change ifmain

--- bars.py
+++ bars.py
@@ -44,23 +44,24 @@ def print_bars_info(bars):
                   
 if __name__ == '__main__':
     
-    if len(sys.argv) > 1:
-        json_input = load_jsonfile(sys.argv[1])
-        if json_input is None:
-            sys.exit(1)
-        bars = [(get_biggest_bar(json_input), 'Biggest'),
-                (get_smallest_bar(json_input), 'Smallest')]
-        if len(sys.argv) > 3:
-            closest_bar = get_closest_bar(json_input, 
-                                        float(sys.argv[2]),
-                                        float(sys.argv[3]))
-            bars.append((closest_bar, 'Nearest'))
-        else:
-            print('for calculating nearest bar: add 2-st argument and 3-st '+ 
-            'argumets: ./python pprint_json.py data.json longitude latitude')
-        print_bars_info(bars)
-        sys.exit(0)
-    else:
+    if len(sys.argv) <= 1:
         print('error: please add valid json file as 1-st argument:'+ 
             ' ./python pprint_json.py data.json longitude latitude')
         sys.exit(1)
+        
+    json_input = load_jsonfile(sys.argv[1])
+    if json_input is None:
+        sys.exit(1)
+    bars = [(get_biggest_bar(json_input), 'Biggest'),
+            (get_smallest_bar(json_input), 'Smallest')]
+    if len(sys.argv) > 3:
+        closest_bar = get_closest_bar(json_input, 
+                                      float(sys.argv[2]),
+                                      float(sys.argv[3]))
+        bars.append((closest_bar, 'Nearest'))
+    else:
+        print('for calculating nearest bar: add 2-st argument and 3-st '+ 
+            'argumets: ./python pprint_json.py data.json longitude latitude')
+    print_bars_info(bars)
+    sys.exit(0)
+        
